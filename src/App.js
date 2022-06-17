934d214948126acec4acbc25be5868a0a949e747
macbook <gioxezz@gmail.com>
2022-06-17 23:40:59 +0400

    finished main page responsiveness

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navigation from './Routes/Navigation/navigation.component';
 import Homepage from './Routes/Homepage/home.component';
 import dumm from './Routes/dummy';
+
 function App() {
     return (
         <div className='main-page-container'>
--- src/Routes/Navigation/navigation.component.jsx
+++ src/Routes/Navigation/navigation.component.jsx
@@ -1,70 +1,172 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import './navigation.styles.scss';
-import { ReactComponent as Logo } from '../../assets/Homepage/Group 2.svg';
+import { ReactComponent as Logo } from '../../assets/shared/logo.svg';
+import { ReactComponent as HamburgerNavbar } from '../../assets/shared/icon-hamburger.svg';
+import { ReactComponent as CancelButton } from '../../assets/shared/icon-close.svg';
 const Navigation = () => {
+    const [mobileNavActive, setMobileNavActive] = useState(false);
+    const [mobileNavState, setMobileNavState] = useState({
+        hamburgerState: true,
+        navClassName: '',
+    });
+
+    useEffect(() => {
+        const windowSize = window.innerWidth;
+        console.log(windowSize);
+        if (windowSize < 608) setMobileNavActive(true);
+    }, []);
+
     return (
         <main>
             <nav className='nav-links-container'>
-                <NavLink className='logo' to='/'>{<Logo />}</NavLink>
-                <ul>
-                    <NavLink
-                        eact
-                        className={({ isActive }) => {
-                            return isActive
-                                ? `nav-link nav-is-active`
-                                : 'nav-link';
-                        }}
-                        to='/'
-                    >
-                        <li>
-                            {' '}
-                            <strong>00</strong>
-                            HOME
-                        </li>
-                    </NavLink>
-                    <NavLink
-                        className={({ isActive }) => {
-                            return isActive
-                                ? `nav-link nav-is-active`
-                                : 'nav-link';
-                        }}
-                        to='/destination'
-                    >
-                        <li>
-                            {' '}
-                            <strong>01</strong>
-                            DESTINATION
-                        </li>
-                    </NavLink>
-                    <NavLink
-                        className={({ isActive }) => {
-                            return isActive
-                                ? `nav-link nav-is-active`
-                                : 'nav-link';
-                        }}
-                        to='/crew'
-                    >
-                        <li>
-                            {' '}
-                            <strong>02</strong>
-                            CREW
-                        </li>
-                    </NavLink>
-                    <NavLink
-                        className={({ isActive }) => {
-                            return isActive
-                                ? `nav-link nav-is-active`
-                                : 'nav-link';
-                        }}
-                        to='/technology'
-                    >
-                        <li>
-                            {' '}
-                            <strong>03</strong>
-                            TECHNOLOGY
-                        </li>
-                    </NavLink>
-                </ul>
+                <NavLink className='logo' to='/'>
+                    {<Logo />}
+                </NavLink>
+                {!mobileNavActive ? (
+                    <ul>
+                        <NavLink
+                            eact
+                            className={({ isActive }) => {
+                                return isActive
+                                    ? `nav-link nav-is-active`
+                                    : 'nav-link';
+                            }}
+                            to='/'
+                        >
+                            <li>
+                                {' '}
+                                <strong>00</strong>
+                                HOME
+                            </li>
+                        </NavLink>
+                        <NavLink
+                            className={({ isActive }) => {
+                                return isActive
+                                    ? `nav-link nav-is-active`
+                                    : 'nav-link';
+                            }}
+                            to='/destination'
+                        >
+                            <li>
+                                {' '}
+                                <strong>01</strong>
+                                DESTINATION
+                            </li>
+                        </NavLink>
+                        <NavLink
+                            className={({ isActive }) => {
+                                return isActive
+                                    ? `nav-link nav-is-active`
+                                    : 'nav-link';
+                            }}
+                            to='/crew'
+                        >
+                            <li>
+                                {' '}
+                                <strong>02</strong>
+                                CREW
+                            </li>
+                        </NavLink>
+                        <NavLink
+                            className={({ isActive }) => {
+                                return isActive
+                                    ? `nav-link nav-is-active`
+                                    : 'nav-link';
+                            }}
+                            to='/technology'
+                        >
+                            <li>
+                                {' '}
+                                <strong>03</strong>
+                                TECHNOLOGY
+                            </li>
+                        </NavLink>
+                    </ul>
+                ) : (
+                    <>
+                        {mobileNavState.hamburgerState ? (
+                            <HamburgerNavbar
+                                className='hamburger-icon'
+                                onClick={() => {
+                                    setMobileNavState({
+                                        navClassName: 'activeNavHamburger',
+                                        hamburgerState: false,
+                                    });
+                                }}
+                            />
+                        ) : (
+                            <CancelButton
+                                className='hamburger-icon'
+                                onClick={() =>
+                                    setMobileNavState({
+                                        hamburgerState: true,
+                                        navClassName: '',
+                                    })
+                                }
+                            />
+                        )}
+                        <ul className={mobileNavState.navClassName}>
+                            <NavLink
+                                eact
+                                className={({ isActive }) => {
+                                    return isActive
+                                        ? `nav-link nav-is-active`
+                                        : 'nav-link';
+                                }}
+                                to='/'
+                            >
+                                <li>
+                                    {' '}
+                                    <strong>00</strong>
+                                    HOME
+                                </li>
+                            </NavLink>
+                            <NavLink
+                                className={({ isActive }) => {
+                                    return isActive
+                                        ? `nav-link nav-is-active`
+                                        : 'nav-link';
+                                }}
+                                to='/destination'
+                            >
+                                <li>
+                                    {' '}
+                                    <strong>01</strong>
+                                    DESTINATION
+                                </li>
+                            </NavLink>
+                            <NavLink
+                                className={({ isActive }) => {
+                                    return isActive
+                                        ? `nav-link nav-is-active`
+                                        : 'nav-link';
+                                }}
+                                to='/crew'
+                            >
+                                <li>
+                                    {' '}
+                                    <strong>02</strong>
+                                    CREW
+                                </li>
+                            </NavLink>
+                            <NavLink
+                                className={({ isActive }) => {
+                                    return isActive
+                                        ? `nav-link nav-is-active`
+                                        : 'nav-link';
+                                }}
+                                to='/technology'
+                            >
+                                <li>
+                                    {' '}
+                                    <strong>03</strong>
+                                    TECHNOLOGY
+                                </li>
+                            </NavLink>
+                        </ul>
+                    </>
+                )}
             </nav>
             <Outlet />
         </main>
